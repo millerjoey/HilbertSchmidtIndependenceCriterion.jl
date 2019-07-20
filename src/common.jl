6894dc78bc5b4441b5de9fb60dec9d6a4cbcce3a
millerjoey <arch1190@gmail.com>
2019-07-20 18:26:14 -0500

    Cleaned up so function can be called with vectors

--- src/common.jl
+++ src/common.jl
@@ -1,37 +1,37 @@
 """
-    estimateKernelSize{T <: Real}(X::Array{T, 2}; sampleSize = 100)
+    estimateKernelSize{T <: Real}(X::Array{T}; sampleSize = 100)
 
     Estimate kernel size by taking median distance between points.
 """
-function estimateKernelSize(X::Array{T, 2}; sampleSize = 100) where T <: Real
+function estimateKernelSize(X::Array{T}; sampleSize = 100) where T <: Real
 
-	M = size(X)[1]
+	M = length(X)
 
 	# set kernel size to median distance between points
 	if M > sampleSize
-		Xmed = X[shuffle(collect(1:M))[1:sampleSize],:]
+		Xmed = reshape(sample(X, sampleSize, replace=false), (sampleSize, 1))
 		S = sampleSize
 	else
-		Xmed = X
+		Xmed = reshape(X, (M, 1))
 		S = M
 	end
 
-	dists = pairwise(SqEuclidean(), Xmed, Xmed, dims = 1) 
+	dists = pairwise(SqEuclidean(), Xmed, Xmed, dims = 1)
     sig = sqrt(0.5 * median(dists))
 
     return sig
 end
 
 """
-    rbfDotProduct(X::Array{T, 2}, X::Array{T, 2}, kernelSize::Float64)
+    rbfDotProduct(X::Array, X::Array, kernelSize::Float64)
 
 """
-function rbfDotProduct(X::Array{T, 2}, Y::Array{T, 2}, kernelSize) where T <: Real
+function rbfDotProduct(X::Array, Y::Array, kernelSize)
 
 	G = sum((X.*Y), dims = 2)
 
-	Q = repeat(G, 1, size(Y, 1))
-	R = repeat(G', size(X, 1), 1)
+	Q = repeat(G, 1, length(Y))
+	R = repeat(G', length(X), 1)
 
 	H = Q + R - 2*X*Y'
 
